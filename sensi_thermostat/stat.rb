16ac25c2908b0f1c17426e43873320a5b9fb593b
Andrew Novak <andrew.novak@target.com>
2018-05-18 20:18:12 -0500

    updated stat collector

--- sensi_thermostat/stat.rb
+++ sensi_thermostat/stat.rb
@@ -6,12 +6,13 @@ class CollectStat
   def main
     device = connect(ENV['sensi_user'],ENV['sensi_pw'])
     db_connect = connect_db(ENV['db'])
+    thermostat_name = device.DeviceName
 
     unless device.nil? || db_connect.nil?
       thermostat_stats = collect_stats(device)
 
       unless thermostat_stats.nil?
-          send_stats(thermostat_stats,db_connect)
+          send_stats(thermostat_stats,db_connect,thermostat_name)
 
       end
     end
@@ -33,32 +34,24 @@ class CollectStat
   end
 
   def collect_stats(thermostat)
-    mode = thermostat.system_mode
-    temp = thermostat.system_temperature
-    humidity = thermostat.humidity
-    on = thermostat.system_on?
-    fan_running = thermostat.system_fan_on?
-    running = thermostat.system_active?
-    runningTime = thermostat.active_time
-
-
     stats = {
-      "mode" => mode,
-      "temp" => temp,
-      "humidity" => humidity,
-      "SystemOn" => on.to_s,
-      "fan_running" => fan_running.to_s,
-      "running" => running.to_s,
-      "runningTime" => runningTime
+      "mode" => thermostat.system_mode,
+      "temp" => thermostat.temperature,
+      "set_temp" => thermostat.system_temperature,
+      "humidity" => thermostat.humidity,
+      "system_enabled" => thermostat.system_on?.to_s,
+      "fan_enabled" => thermostat.system_fan_on?.to_s,
+      "System_running" => thermostat.system_active?.to_s,
+      "runtime" => thermostat.active_time
     }
     stats
   end
 
-  def send_stats(stats,db)
-    name = 'thermostat'
+  def send_stats(stats,db,name)
+    name = 'home_furnace'
 
     stats.each do |key,value|
-      data = {values: { value: "#{value}" },tags:{ stat: "#{key}",location: "upstairs" }}
+      data = {values: { "#{{key}}": "#{value}" },tags:{ location: "#{{name}}" }}
       db.write_point(name, data)
       #puts data
     end
